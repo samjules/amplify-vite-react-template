cfdacf9850f7bb6a899891ce97c030649111af51
samjules <samjulander@gmail.com>
2025-02-08 09:07:34 -0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>User todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
